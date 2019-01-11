34a12ddd0ea9b50f1ed0146c6a7c79821ccabd86
Hyuk Gi Lee <hyukgilee@D-10-156-17-229.dhcp4.washington.edu>
2019-01-10 19:55:15 -0800

    initialcommit

---
+++ main/main/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  main
+//
+//  Created by Hyuk Gi Lee on 1/10/19.
+//  Copyright © 2019 Hyuk Gi Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
